029d5cf5af2e357a7bc311fb57a62fe270f0e50a
Juan Pablo Ramirez Vega <juparave@deuxbits.com>
2024-05-16 09:35:50 -0600

    only is we get `cpuStat` info

--- internal/hardware/hardware.go
+++ internal/hardware/hardware.go
@@ -5,6 +5,7 @@ import (
 	"github.com/shirou/gopsutil/disk"
 	"github.com/shirou/gopsutil/host"
 	"github.com/shirou/gopsutil/mem"
+	"fmt"
 	"runtime"
 	"strconv"
 )
@@ -55,6 +56,10 @@ func GetDiskSection() (string, error) {
 
 func GetCpuSection() (string, error) {
 	cpuStat, err := cpu.Info()
+	if err != nil {
+		fmt.Println("Error getting CPU info", err)
+
+	}
 	percentage, err := cpu.Percent(0, true)
 	if err != nil {
 		return "", err
@@ -62,9 +67,11 @@ func GetCpuSection() (string, error) {
 
 	html := "<div class='cpu-data'><table class='table table-striped table-hover table-sm'><tbody>"
 
-	html = html + "<tr><td>Model Name:</td><td>" + cpuStat[0].ModelName + "</td></tr>"
-	html = html + "<tr><td>Family:</td><td>" + cpuStat[0].Family + "</td></tr>"
-	html = html + "<tr><td>Speed:</td><td>" + strconv.FormatFloat(cpuStat[0].Mhz, 'f', 2, 64) + " MHz</td></tr>"
+	if len(cpuStat) != 0 {
+		html = html + "<tr><td>Model Name:</td><td>" + cpuStat[0].ModelName + "</td></tr>"
+		html = html + "<tr><td>Family:</td><td>" + cpuStat[0].Family + "</td></tr>"
+		html = html + "<tr><td>Speed:</td><td>" + strconv.FormatFloat(cpuStat[0].Mhz, 'f', 2, 64) + " MHz</td></tr>"
+	}
 
 	firstCpus := percentage[:len(percentage)/2]
 	secondCpus := percentage[len(percentage)/2:]
